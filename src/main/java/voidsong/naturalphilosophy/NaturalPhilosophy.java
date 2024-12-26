f5e806927bc1546b328dfc0a259e095cc11b401a
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2024-12-26 15:39:05 -0500

    Clean up NaturalPhilosophy.java

--- src/main/java/voidsong/naturalphilosophy/NaturalPhilosophy.java
+++ src/main/java/voidsong/naturalphilosophy/NaturalPhilosophy.java
@@ -8,18 +8,10 @@ import static voidsong.naturalphilosophy.common.NPItems.CREATIVE_MODE_TABS;
 import static voidsong.naturalphilosophy.common.NPItems.ITEMS;
 import static voidsong.naturalphilosophy.common.NPFeatures.FEATURES;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(NaturalPhilosophy.MODID)
 public class NaturalPhilosophy {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "naturalphilosophy";
-
-
-    /*
-     // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
-            .alwaysEat().nutrition(1).saturationMod(2f).build()));
-    */
 
     public NaturalPhilosophy(IEventBus modEventBus) {
         // Register mod content
