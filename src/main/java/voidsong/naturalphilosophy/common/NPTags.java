73b67095f9102e74e9e136a20121a49551f32333
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-01-24 15:24:31 -0500

    Prep for water plant blocks
     - Create tag for water plant survivability
     - Create "cross cutout" template model

---
+++ src/main/java/voidsong/naturalphilosophy/common/NPTags.java
@@ -0,0 +1,19 @@
+package voidsong.naturalphilosophy.common;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import voidsong.naturalphilosophy.NaturalPhilosophy;
+
+import javax.annotation.Nonnull;
+
+public class NPTags {
+    public static class Blocks {
+        public static final TagKey<Block> WATER_PLANTS = tag("water_plant_survivable");
+
+        private static TagKey<Block> tag(@Nonnull String name) {
+            return BlockTags.create(new ResourceLocation(NaturalPhilosophy.MODID, name));
+        }
+    }
+}
