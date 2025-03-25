270e92ed38ff738d5a514612756d30b999ff57f4
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-03-24 22:17:49 -0400

    Fix significant Mixin issues with help from WireSegal

--- src/main/java/voidsong/naturalphilosophy/mixin/FreezeSurfaceMixin.java
+++ src/main/java/voidsong/naturalphilosophy/mixin/FreezeSurfaceMixin.java
@@ -1,53 +1,18 @@
 package voidsong.naturalphilosophy.mixin;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.WorldGenLevel;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SnowyDirtBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.SnowAndFreezeFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @SuppressWarnings("unused")
 @Mixin(SnowAndFreezeFeature.class)
 public abstract class FreezeSurfaceMixin {
 
-    @Overwrite
-    public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
-        WorldGenLevel worldgenlevel = context.level();
-        BlockPos blockpos = context.origin();
-        BlockPos.MutableBlockPos top = new BlockPos.MutableBlockPos();
-        BlockPos.MutableBlockPos surface = new BlockPos.MutableBlockPos();
-
-        for(int i = 0; i < 16; ++i) {
-            for(int j = 0; j < 16; ++j) {
-                int k = blockpos.getX() + i;
-                int l = blockpos.getZ() + j;
-                int i1 = worldgenlevel.getHeight(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, k, l);
-                top.set(k, i1, l);
-                surface.set(top).move(Direction.DOWN, 1);
-                Biome biome = worldgenlevel.getBiome(top).value();
-                if (biome.shouldFreeze(worldgenlevel, surface, false)) {
-                    worldgenlevel.setBlock(surface, Blocks.ICE.defaultBlockState(), 2);
-                }
-
-                if (biome.shouldSnow(worldgenlevel, top)) {
-                    worldgenlevel.setBlock(top, Blocks.SNOW.defaultBlockState(), 2);
-                    BlockState blockstate = worldgenlevel.getBlockState(surface);
-                    if (blockstate.hasProperty(SnowyDirtBlock.SNOWY)) {
-                        worldgenlevel.setBlock(surface, blockstate.setValue(SnowyDirtBlock.SNOWY, Boolean.TRUE), 2);
-                    }
-                }
-            }
-        }
-
-        return true;
+    @ModifyArg(method = "place", at = @At(value = "INVOKE", target = "net/minecraft/world/level/WorldGenLevel.getHeight(Lnet/minecraft/world/level/levelgen/Heightmap$Types;II)I"), index = 0)
+    private Heightmap.Types useCorrectHeightmapType(Heightmap.Types original, int koriginal, int loriginal) {
+        return original == Heightmap.Types.MOTION_BLOCKING ? Heightmap.Types.MOTION_BLOCKING_NO_LEAVES : original;
     }
 }
 
--- src/main/java/voidsong/naturalphilosophy/mixin/SnowyTreesMixin.java
+++ src/main/java/voidsong/naturalphilosophy/mixin/SnowyTreesMixin.java
@@ -1,7 +1,8 @@
 package voidsong.naturalphilosophy.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
@@ -11,25 +12,24 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Set;
 
 /**
  * Inspired by code from Apollounknowndev in Simple Snowy Fix, and updated to 1.20.4
+ * Cleaned up with help from wiresegal re: LocalCapture vs @Local
  */
 @SuppressWarnings("unused")
 @Mixin(TreeFeature.class)
 public class SnowyTreesMixin {
     @Inject(
         method = "place(Lnet/minecraft/world/level/levelgen/feature/FeaturePlaceContext;)Z",
-        locals = LocalCapture.CAPTURE_FAILHARD,
         at = @At(value= "INVOKE", target = "Lnet/minecraft/world/level/levelgen/structure/BoundingBox;encapsulatingPositions(Ljava/lang/Iterable;)Ljava/util/Optional;", shift = At.Shift.BEFORE)
     )
-    private void snowOnLeaves(FeaturePlaceContext<TreeConfiguration> context, CallbackInfoReturnable<Boolean> cir, WorldGenLevel level, RandomSource random, BlockPos pos, TreeConfiguration treeConfiguration, Set<BlockPos> set, Set<BlockPos> set2, Set<BlockPos> set3, Set<BlockPos> set4) {
-        for (BlockPos leaves : set3) {
+    private void snowOnLeaves(FeaturePlaceContext<TreeConfiguration> context, CallbackInfoReturnable<Boolean> cir, @Local WorldGenLevel level, @Local(ordinal = 2) Set<BlockPos> set2) {
+        for (BlockPos leaves : set2) {
             BlockPos top = leaves.above();
-            if (level.isEmptyBlock(top) && level.getBiome(top).value().shouldSnow(level, top)) {
+            if (level instanceof WorldGenRegion && level.isEmptyBlock(top) && !level.isEmptyBlock(leaves) && level.getBiome(top).value().shouldSnow(level, top)) {
                 level.setBlock(top, Blocks.SNOW.defaultBlockState(), 19);
             }
         }
