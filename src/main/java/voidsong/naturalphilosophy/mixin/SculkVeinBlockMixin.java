eb69f3690d0ea009e439161f3c6188a18e4d85b0
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-06-07 21:07:10 -0400

    Dry sculk & glow lichen do not obstruct lava flow
     - This required several overwrite mixins due to the nature of the task
     - They behave like normal waterlogged blocks when waterlogged

---
+++ src/main/java/voidsong/naturalphilosophy/mixin/SculkVeinBlockMixin.java
@@ -0,0 +1,41 @@
+package voidsong.naturalphilosophy.mixin;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.SculkVeinBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.LavaFluid;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.Nullable;
+
+@Mixin(SculkVeinBlock.class)
+@SuppressWarnings("unused")
+public class SculkVeinBlockMixin {
+    public boolean canPlaceLiquid(@Nullable Player player, BlockGetter level, BlockPos pos, BlockState state, Fluid fluid) {
+        return fluid == Fluids.WATER || ((fluid == Fluids.LAVA || fluid == Fluids.FLOWING_LAVA) && !state.getValue(BlockStateProperties.WATERLOGGED));
+    }
+
+    public boolean placeLiquid(LevelAccessor level, BlockPos pos, BlockState state, FluidState fluidState) {
+        if (!state.getValue(BlockStateProperties.WATERLOGGED) && fluidState.getType() == Fluids.WATER) {
+            if (!level.isClientSide()) {
+                level.setBlock(pos, state.setValue(BlockStateProperties.WATERLOGGED, Boolean.TRUE), 3);
+                level.scheduleTick(pos, fluidState.getType(), fluidState.getType().getTickDelay(level));
+            }
+            return true;
+        } else if (fluidState.is(Fluids.LAVA) || fluidState.is(Fluids.FLOWING_LAVA)){
+            Fluid type = fluidState.getType();
+            if (type instanceof LavaFluid lava)
+                lava.beforeDestroyingBlock(level, pos, state);
+            level.setBlock(pos, fluidState.createLegacyBlock(), 3);
+            return true;
+        } else
+            return false;
+    }
+}
