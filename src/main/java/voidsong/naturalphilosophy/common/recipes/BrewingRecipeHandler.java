5c04b6ebbc96052072853950ec535878b8d7329c
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-07-08 18:35:21 -0400

    Add in brewing recipes for Water Breathing using Red Algae

---
+++ src/main/java/voidsong/naturalphilosophy/common/recipes/BrewingRecipeHandler.java
@@ -0,0 +1,19 @@
+package voidsong.naturalphilosophy.common.recipes;
+
+import net.minecraft.world.item.alchemy.PotionBrewing;
+import net.minecraft.world.item.alchemy.Potions;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.brewing.RegisterBrewingRecipesEvent;
+import voidsong.naturalphilosophy.NaturalPhilosophy;
+import voidsong.naturalphilosophy.common.NPItems;
+
+@EventBusSubscriber(modid = NaturalPhilosophy.MODID, bus = EventBusSubscriber.Bus.GAME)
+public class BrewingRecipeHandler {
+    @SubscribeEvent
+    public static void registerBrewingRecipes(RegisterBrewingRecipesEvent event) {
+        PotionBrewing.Builder builder = event.getBuilder();
+
+        builder.addMix(Potions.AWKWARD, NPItems.RED_ALGAE_ITEM.get(), Potions.WATER_BREATHING);
+    }
+}
