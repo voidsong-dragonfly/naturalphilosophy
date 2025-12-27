8ff800a2f2b86c55ecc4aa41fa4012f3f38b2f5b
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-12-27 18:57:22 -0500

    Comment out ice config for now as snow will need to wait
     - Snow is a "breaking" change that will come in 4.0.0
     - Default states mean it looks bad without new world

--- src/main/java/voidsong/naturalphilosophy/common/config/NPServerConfig.java
+++ src/main/java/voidsong/naturalphilosophy/common/config/NPServerConfig.java
@@ -4,13 +4,13 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class NPServerConfig {
     // Configs
-    public static final ModConfigSpec.IntValue maxSnowIceWaterDepth;
+    //public static final ModConfigSpec.IntValue maxSnowIceWaterDepth;
     // Init & technical variables
     public static final ModConfigSpec CONFIG_SPEC;
     static
     {
         ModConfigSpec.Builder builder = new ModConfigSpec.Builder();
-        maxSnowIceWaterDepth = builder.comment("Maximum water depth that snow will cover ice").defineInRange("maxSnowIceWaterDepth", 8, 0, 64);
+        //maxSnowIceWaterDepth = builder.comment("Maximum water depth that snow will cover ice").defineInRange("maxSnowIceWaterDepth", 8, 0, 64);
         CONFIG_SPEC = builder.build();
     }
 }
