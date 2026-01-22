690e3b81f4aed9053a7fbef55917b76c1bb31a1f
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-01-22 11:36:48 -0500

    First attempt at making bamboo look better
     - Bamboo now has bushy leaves at the top
     - Small and large leaves are enhanced in size
     - No part of bamboo is unshaded anymore
     - Having leaves now reduces skylight values by 1, stalks still reduce skylight by 0 as with before

---
+++ src/main/java/voidsong/naturalphilosophy/mixin/blockmodifications/BambooMixin.java
@@ -0,0 +1,27 @@
+package voidsong.naturalphilosophy.mixin.blockmodifications;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.BambooStalkBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BambooLeaves;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.Nonnull;
+
+@Mixin(BambooStalkBlock.class)
+public class BambooMixin extends Block {
+    /**
+     * This constructor is the default & will be ignored, it exists so we can extend Block
+     * @param properties ignored & should not be used!
+     */
+    public BambooMixin(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected int getLightBlock(@Nonnull BlockState state, @Nonnull BlockGetter level, @Nonnull BlockPos pos) {
+        return state.hasProperty(BambooStalkBlock.LEAVES) && !state.getValue(BambooStalkBlock.LEAVES).equals(BambooLeaves.NONE) ? 1 : 0;
+    }
+}
