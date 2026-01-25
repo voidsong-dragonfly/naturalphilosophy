7eab21d68c82905d29f5e09615f6f8db854cdd6b
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-01-24 23:13:15 -0500

    Finish up flower biome tinting
     - Most flowers spawn in clusters due to the issue of MC flower sizes vs a 16x texture
     - Only ones that don't are azure bluets/blue orchids
     - Those have bushes and more stalks respectively
     - All flowers have the same stalk coloration

--- src/main/java/voidsong/naturalphilosophy/client/ColorHandler.java
+++ src/main/java/voidsong/naturalphilosophy/client/ColorHandler.java
@@ -27,6 +27,7 @@ public class ColorHandler {
             Blocks.PEONY, Blocks.ROSE_BUSH, Blocks.LILAC, Blocks.SUNFLOWER);
         event.register((state, world, pos, tintIndex) ->
                 world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(),
-            Blocks.LILY_OF_THE_VALLEY, Blocks.ALLIUM, Blocks.RED_TULIP, Blocks.PINK_TULIP, Blocks.ORANGE_TULIP, Blocks.WHITE_TULIP);
+            Blocks.LILY_OF_THE_VALLEY, Blocks.ALLIUM, Blocks.RED_TULIP, Blocks.PINK_TULIP, Blocks.ORANGE_TULIP, Blocks.WHITE_TULIP,
+            Blocks.POPPY, Blocks.DANDELION, Blocks.CORNFLOWER, Blocks.OXEYE_DAISY, Blocks.BLUE_ORCHID, Blocks.AZURE_BLUET);
     }
 }
