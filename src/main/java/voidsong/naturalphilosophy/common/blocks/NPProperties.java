d54a4d5000b4c8b03fe3c11404873bffc0bddb11
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-01-27 15:40:51 -0500

    Remove unused property file

--- src/main/java/voidsong/naturalphilosophy/common/blocks/NPProperties.java
+++
@@ -1,8 +0,0 @@
-package voidsong.naturalphilosophy.common.blocks;
-
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-
-public class NPProperties {
-    public static final BooleanProperty RED_SAND = BooleanProperty.create("red_sand");
-
-}
