c90c91a7b87e4366e7af3e38cbaeca318905fccc
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-02-27 17:18:29 -0500

    Fixes #10 by allowing rushes to spread
     - Rushes/Cattails spread when bonemealed
     - Will only spread to valid blocks
     - Will generate ~5 per bonemeal

--- src/main/java/voidsong/naturalphilosophy/common/blocks/WaterPlantBlock.java
+++ src/main/java/voidsong/naturalphilosophy/common/blocks/WaterPlantBlock.java
@@ -2,14 +2,13 @@ package voidsong.naturalphilosophy.common.blocks;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.DoublePlantBlock;
-import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
@@ -21,7 +20,7 @@ import voidsong.naturalphilosophy.common.NPTags;
 
 import javax.annotation.Nonnull;
 
-public class WaterPlantBlock extends DoublePlantBlock implements SimpleWaterloggedBlock {
+public class WaterPlantBlock extends DoublePlantBlock implements SimpleWaterloggedBlock, BonemealableBlock {
 
     public WaterPlantBlock(BlockBehaviour.Properties props) {
         super(props);
@@ -68,11 +67,33 @@ public class WaterPlantBlock extends DoublePlantBlock implements SimpleWaterlogg
        return null;
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     @Nonnull
     public FluidState getFluidState(BlockState state) {
         return state.getValue(BlockStateProperties.WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
+    @Override
+    public boolean isValidBonemealTarget(@Nonnull LevelReader level, @Nonnull BlockPos pos, @Nonnull BlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isBonemealSuccess(@Nonnull Level level, @Nonnull RandomSource random, @Nonnull BlockPos pos, @Nonnull BlockState state) {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(@Nonnull ServerLevel level, @Nonnull RandomSource random, @Nonnull BlockPos pos, @Nonnull BlockState state) {
+        BlockPos center = state.getValue(HALF).equals(DoubleBlockHalf.UPPER) ? pos.below() : pos;
+        for (BlockPos search : BlockPos.betweenClosed(center.offset(-2, -1, -2), center.offset(2, 1, 2))) {
+            boolean water = level.getBlockState(search).is(Blocks.WATER);
+            boolean bottomValid = this.canSurvive(this.defaultBlockState(), level, search) && (level.getBlockState(search).isAir() || water);
+            boolean topValid = level.getBlockState(search.above()).isAir();
+            if (bottomValid && topValid && random.nextInt(5) == 0) {
+                level.setBlockAndUpdate(search, defaultBlockState().setValue(BlockStateProperties.WATERLOGGED, water));
+                level.setBlockAndUpdate(search.above(), defaultBlockState().setValue(HALF, DoubleBlockHalf.UPPER));
+            }
+        }
+    }
 }
