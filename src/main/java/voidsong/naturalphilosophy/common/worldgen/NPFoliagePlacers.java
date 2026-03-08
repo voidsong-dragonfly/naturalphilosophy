f1f8c67bd91e979924df33b0ed25ba890e2668ba
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-03-08 18:46:01 -0400

    Add in red mushroom cap foliage placer

--- src/main/java/voidsong/naturalphilosophy/common/worldgen/NPFoliagePlacers.java
+++ src/main/java/voidsong/naturalphilosophy/common/worldgen/NPFoliagePlacers.java
@@ -7,6 +7,7 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 import voidsong.naturalphilosophy.NaturalPhilosophy;
 import voidsong.naturalphilosophy.common.worldgen.foliageplacers.BranchedMegaJungleFoliagePlacer;
 import voidsong.naturalphilosophy.common.worldgen.foliageplacers.BranchedMegaPineFoliagePlacer;
+import voidsong.naturalphilosophy.common.worldgen.foliageplacers.RedMushroomCapFoliagePlacer;
 import voidsong.naturalphilosophy.common.worldgen.foliageplacers.RoundedBlobFoliagePlacer;
 
 public class NPFoliagePlacers {
@@ -15,4 +16,5 @@ public class NPFoliagePlacers {
     public static final DeferredHolder<FoliagePlacerType<?>, FoliagePlacerType<BranchedMegaPineFoliagePlacer>> BRANCHED_MEGA_PINE = FOLIAGE_PLACERS.register("branched_mega_pine_foliage_placer", () -> new FoliagePlacerType<>(BranchedMegaPineFoliagePlacer.CODEC));
     public static final DeferredHolder<FoliagePlacerType<?>, FoliagePlacerType<BranchedMegaJungleFoliagePlacer>> BRANCHED_MEGA_JUNGLE = FOLIAGE_PLACERS.register("branched_jungle_foliage_placer", () -> new FoliagePlacerType<>(BranchedMegaJungleFoliagePlacer.CODEC));
     public static final DeferredHolder<FoliagePlacerType<?>, FoliagePlacerType<RoundedBlobFoliagePlacer>> ROUNDED_BLOB = FOLIAGE_PLACERS.register("rounded_blob_foliage_placer", () -> new FoliagePlacerType<>(RoundedBlobFoliagePlacer.CODEC));
+    public static final DeferredHolder<FoliagePlacerType<?>, FoliagePlacerType<RedMushroomCapFoliagePlacer>> RED_MUSHROOM_CAP = FOLIAGE_PLACERS.register("red_mushroom_cap_foliage_placer", () -> new FoliagePlacerType<>(RedMushroomCapFoliagePlacer.CODEC));
 }
