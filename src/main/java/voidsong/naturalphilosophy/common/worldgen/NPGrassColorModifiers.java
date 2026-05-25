30d11c2a54ff0da3722abb0b8ba9c304bff6dc32
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-05-25 01:14:53 -0400

    Fix jungle overstory water depth checks

--- src/main/java/voidsong/naturalphilosophy/common/worldgen/NPGrassColorModifiers.java
+++ src/main/java/voidsong/naturalphilosophy/common/worldgen/NPGrassColorModifiers.java
@@ -6,17 +6,16 @@ import net.neoforged.fml.common.asm.enumextension.EnumProxy;
 
 @SuppressWarnings("unused")
 public class NPGrassColorModifiers {
-    @SuppressWarnings("ExtractMethodRecommender")
     public static final EnumProxy<BiomeSpecialEffects.GrassColorModifier> LUSH_SWAMP = new EnumProxy<>(
         BiomeSpecialEffects.GrassColorModifier.class, "naturalphilosophy:lush_swamp", (BiomeSpecialEffects.GrassColorModifier.ColorModifier)
             (double x, double z, int color) -> {
                 // Get the part of the color we actually care about and remove the rest
                 int extracted_color = color & 16777215;
+                int[] split = {((extracted_color >>> 16) & 255), ((extracted_color >>> 8) & 255), (extracted_color & 255)};
                 // Get the biome noise
                 double d0 = Biome.BIOME_INFO_NOISE.getValue(x * 0.0225, z * 0.0225, false);
                 // Modify the color in the same way the swamp color does it
                 // We move each color (R, G, B) by the fraction from the swamp "average" it was moved, then make sure it's within bounds
-                int[] split = {((extracted_color >>> 16) & 255), ((extracted_color >>> 8) & 255), (extracted_color & 255)};
                 if (d0 < -0.1) {
                     split = new int[]{(int)(split[0] - (split[0]*0.1648))&255, (int)(split[1] - (split[1]*0.0261))&255, (int)(split[2] - (split[2]*0.0256))&255};
                 } else {
