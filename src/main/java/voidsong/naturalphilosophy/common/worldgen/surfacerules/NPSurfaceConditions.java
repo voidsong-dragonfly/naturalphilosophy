ab82e64115573a9ee9834894d187139d24212a64
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-06-28 21:04:08 -0400

    Tweaks to CliffCondition
     - Integrated current block height into slope
     - Better edge/lip detection
        - Uses top block check properly
        - Removed grass in the middle of cliffs
        - Some cave entrances are not perfect
     - Improved edge detection for edge bottoms
        - No longer triggers on big 3D cliffs
        - Checks relative to top block of column

--- src/main/java/voidsong/naturalphilosophy/common/worldgen/surfacerules/NPSurfaceConditions.java
+++ src/main/java/voidsong/naturalphilosophy/common/worldgen/surfacerules/NPSurfaceConditions.java
@@ -26,18 +26,29 @@ public class NPSurfaceConditions {
             int eastHeight  = chunk.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, east, j);
             int southHeight = chunk.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, i, south);
             int westHeight  = chunk.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, west, j);
-            // Get the height difference we need to check to ensure this is a cliff
-            int difference = (Math.max(northHeight, Math.max(eastHeight, Math.max(southHeight, westHeight))) - Math.min(northHeight, Math.min(eastHeight, Math.min(southHeight, westHeight))));
-            // Exit early, to ensure we don't make the more intensive checks.
-            if (difference < 3) return false;
-            // Check that we're not on a cliff top-lip. Since the scan is top-down, this catches tops first despite being LazyXZ TODO: possibly not make this apply to big cliff-sides
-            boolean lip = this.context.stoneDepthBelow <= 2;
-            // Check that we're not at the bottom of a hanging-over cave entrance. These air checks function because, and so this catches the top despite being LazyXZ
-            lip = lip || chunk.getBlockState(new BlockPos(this.context.blockX, this.context.blockY+2, this.context.blockZ-j+north)).isAir() && this.context.blockY+2 < northHeight;
-            lip = lip || chunk.getBlockState(new BlockPos(this.context.blockX-i+east, this.context.blockY+2, this.context.blockZ)).isAir() && this.context.blockY+2 < eastHeight;
-            lip = lip || chunk.getBlockState(new BlockPos(this.context.blockX, this.context.blockY+2, this.context.blockZ-j+south)).isAir() && this.context.blockY+2 < southHeight;
-            lip = lip || chunk.getBlockState(new BlockPos(this.context.blockX-i+west, this.context.blockY+2, this.context.blockZ)).isAir() && this.context.blockY+2 < westHeight;
-            return !lip;
+            // Get adjacent heightmap min & max heights. We include this block in max but not min to keep spires but not pits as stone
+            int min = Math.min(northHeight, Math.min(eastHeight, Math.min(southHeight, westHeight)));
+            int maxAdjacent = min;
+            if (j != north) maxAdjacent = northHeight;
+            if (i != east)  maxAdjacent = Math.max(maxAdjacent, eastHeight);
+            if (j != south) maxAdjacent = Math.max(maxAdjacent, southHeight);
+            if (i != west)  maxAdjacent = Math.max(maxAdjacent, westHeight);
+            int y = context.blockY + context.stoneDepthAbove - 1;
+            int max = Math.max(y, maxAdjacent);
+            // Use the height difference we need to check to ensure this is a cliff (max - min >= 3) or a one-block spire (max > maxAdjacent)
+            if ((max - min) < 3 && !(max > maxAdjacent && (max - min) == 2)) return false;
+            // Return if we're on a cliff lip (low depth, flat height profile) based on the top of this block column if run at the surface
+            // Note: this check will cause strange behavior if it is not run in the top "section" first to allow LazyXZ to cache it for the entire column
+            if (this.context.stoneDepthBelow <= 3 && (max - y < 2))  return false;
+            // Return if we're at in the middle of a large cliff, regardless of depth, to prevent 3D cliffs from having grass streaks due to lip-like behavior
+            if ((max - y) > 11 && (y - min) > 1) return true;
+            // Check that we're not at the bottom of a hanging-over cave entrance by checking if there's air pockets beneath ground height on the sides
+            // Note: this has the same strange behavior & cause as the previous lip check does, it should be run in the top "section" first and allowed to cache itself
+            boolean floor =  chunk.getBlockState(new BlockPos(this.context.blockX, y+2, this.context.blockZ-j+north)).isAir() && y+2 < northHeight;
+            floor = floor || chunk.getBlockState(new BlockPos(this.context.blockX-i+east, y+2, this.context.blockZ)).isAir() && y+2 < eastHeight;
+            floor = floor || chunk.getBlockState(new BlockPos(this.context.blockX, y+2, this.context.blockZ-j+south)).isAir() && y+2 < southHeight;
+            floor = floor || chunk.getBlockState(new BlockPos(this.context.blockX-i+west, y+2, this.context.blockZ)).isAir() && y+2 < westHeight;
+            return !floor;
         }
     }
 
