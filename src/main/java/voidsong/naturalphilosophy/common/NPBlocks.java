2483b8a269c9fecefd78fa7b8b4170f969cf1e41
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-07-28 00:04:48 -0400

    Adjustments to rushes/cattails models
     - Removed AO from these models as well
     - Touched up bottom textures to flow better, rather than having jarring transitions in texture color saturation
     - Reduced texture size in atlas by re-using textures for rushes
     - Made rush textures for seed tops less strange-looking and overhanging
     - Rushes & cattails are `OffsetType.XZ` now to match other tall plants

--- src/main/java/voidsong/naturalphilosophy/common/NPBlocks.java
+++ src/main/java/voidsong/naturalphilosophy/common/NPBlocks.java
@@ -49,8 +49,8 @@ public class NPBlocks {
 
     public static final DeferredBlock<Block> DUNE_GRASS = BLOCKS.registerBlock("dune_grass", DuneGrass::new, GRASS_PROPERTIES);
     public static final DeferredBlock<Block> TALL_DUNE_GRASS = BLOCKS.registerBlock("tall_dune_grass", TallDuneGrass::new, TALL_GRASS_PROPERTIES);
-    public static final DeferredBlock<Block> RUSHES = BLOCKS.registerBlock("rushes", WaterPlantBlock::new, GRASS_PROPERTIES);
-    public static final DeferredBlock<Block> CATTAILS = BLOCKS.registerBlock("cattails", WaterPlantBlock::new, GRASS_PROPERTIES);
+    public static final DeferredBlock<Block> RUSHES = BLOCKS.registerBlock("rushes", WaterPlantBlock::new, TALL_GRASS_PROPERTIES);
+    public static final DeferredBlock<Block> CATTAILS = BLOCKS.registerBlock("cattails", WaterPlantBlock::new, TALL_GRASS_PROPERTIES);
     public static final DeferredBlock<Block> MYCELIAL_GROWTH = BLOCKS.registerBlock("mycelial_growth", MycelialGrowthBlock::new,
         BlockBehaviour.Properties.of()
             .mapColor(MapColor.COLOR_PURPLE)
