664e14ef276bc287841784c3a6dc8c4d8f0e1a69
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2026-08-04 20:43:56 -0400

    Make Mycelial Growth no longer a `RootsBlock`
     - Does not inherit from nether roots
     - Cannot be placed on Nylium or soul sand
     - Can still be placed on `#minecraft:dirt`

--- src/main/java/voidsong/naturalphilosophy/common/blocks/MycelialGrowthBlock.java
+++ src/main/java/voidsong/naturalphilosophy/common/blocks/MycelialGrowthBlock.java
@@ -1,17 +1,39 @@
 package voidsong.naturalphilosophy.common.blocks;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.RootsBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nonnull;
 
-public class MycelialGrowthBlock extends RootsBlock {
+public class MycelialGrowthBlock extends BushBlock {
+    public static final MapCodec<RootsBlock> CODEC = simpleCodec(RootsBlock::new);
+    protected static final VoxelShape SHAPE = Block.box(2.0, 0.0, 2.0, 14.0, 13.0, 14.0);
+
+    @Override
+    @Nonnull
+    public MapCodec<RootsBlock> codec() {
+        return CODEC;
+    }
+
     public MycelialGrowthBlock(BlockBehaviour.Properties properties) {
         super(properties);
+    }
+
+    @Override
+    @Nonnull
+    protected VoxelShape getShape(@Nonnull BlockState state, @Nonnull BlockGetter level, @Nonnull BlockPos pos, @Nonnull CollisionContext context) {
+        Vec3 vec3 = state.getOffset(level, pos);
+        return SHAPE.move(vec3.x, vec3.y, vec3.z);
     }
 
     @Override
